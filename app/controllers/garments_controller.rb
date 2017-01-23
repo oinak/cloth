d491cced0b32c7d8a7a5a7c0c8b005d64eeaea0c
Fernando Martínez <fernando.mdelacueva@gmail.com>
2017-01-23 16:11:21 +0100

    Basic garment admin REST controller

--- app/controllers/garments_controller.rb
+++ app/controllers/garments_controller.rb
@@ -1,9 +1,47 @@
 class GarmentsController < ApplicationController
+  before_action :set_garment, only: %i(new create edit update destroy)
   # before_action :require_login
 
   def index
     @garments = Garment.all
   end
 
+  def new
+  end
 
+  def create
+    if @garment.save
+      redirect_to garments_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @garment.save
+      redirect_to garments_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @garment.destroy if @garment
+    redirect_to garments_path
+  end
+
+  private
+
+  def set_garment
+    @garment = params[:id] ? Garment.find_by(id: params[:id]) : Garment.new
+    @garment.assign_attributes(garment_params) if @garment && params[:garment]
+  end
+
+  def garment_params
+    params.require(:garment)
+          .permit(:name, :sku, :color, :kind, :size, :price, :brand)
+  end
 end
